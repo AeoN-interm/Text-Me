a2d26b9f690446ceced9cd90d3933748583c22e3
Saikat biswas <niswassaikatt@gmail.com>
2025-05-13 21:42:14 +0530

    Initial commit

---
+++ backend/data/data.js
@@ -0,0 +1,108 @@
+const chats = [
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Ram",
+        email: "Ram@example.com",
+      },
+      {
+        name: "Saikat",
+        email: "Saikat@example.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7c4dd4",
+    chatName: "Ram",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Guest User",
+        email: "guest@example.com",
+      },
+      {
+        name: "Saikat",
+        email: "Saikat@example.com",
+      },
+    ],
+    _id: "617a077e18c25468b27c4dd4",
+    chatName: "Guest User",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Lakshman",
+        email: "Lakshman@example.com",
+      },
+      {
+        name: "Saikat",
+        email: "Saikat@example.com",
+      },
+    ],
+    _id: "617a077e18c2d468bc7c4dd4",
+    chatName: "Lakshman",
+  },
+  {
+    isGroupChat: true,
+    users: [
+      {
+        name: "Ram",
+        email: "Ram@example.com",
+      },
+      {
+        name: "Saikat",
+        email: "Saikat@example.com",
+      },
+      {
+        name: "Guest User",
+        email: "guest@example.com",
+      },
+    ],
+    _id: "617a518c4081150716472c78",
+    chatName: "Friends",
+    groupAdmin: {
+      name: "Guest User",
+      email: "guest@example.com",
+    },
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Ram",
+        email: "Ram@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7cfdd4",
+    chatName: "Jane Doe",
+  },
+  {
+    isGroupChat: true,
+    users: [
+      {
+        name: "Ram",
+        email: "Ram@example.com",
+      },
+      {
+        name: "Saikat",
+        email: "Saikat@example.com",
+      },
+      {
+        name: "Guest User",
+        email: "guest@example.com",
+      },
+    ],
+    _id: "617a518c4081150016472c78",
+    chatName: "Chill Zone",
+    groupAdmin: {
+      name: "Guest User",
+      email: "guest@example.com",
+    },
+  },
+];
